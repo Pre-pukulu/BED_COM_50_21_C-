1747882f5008c4eb602220b2bcc0fc06ae97da92
Pre-pukulu <167034442+Pre-pukulu@users.noreply.github.com>
2024-05-16 07:08:33 +0200

    the rectangle.h file was using wrong declaration

--- PROJECTS/LAB 7/Rectangle.h
+++ PROJECTS/LAB 7/Rectangle.h
@@ -1,43 +1,28 @@
-// #pragma once
-
-// class Rectangle
-// {
-//     //private member variables
-//     private:
-//     float length;
-//     float width;
-
-
-//     public:
-    
-//     //default constructor intitilizing variables to 0
-//     Rectangle();
-
-//     ~Rectangle();
-//     //accesor method for assigning vlues
-//     void setVAriables(float newlength, float newWidth);
-
-//     //accesor methods for retriving values of length and withdth
-//     float getlength() const;
-
-//     float getwidth() const;
-
-//     //function for calculating  area of rectangle
-//     float CalculateArea(float len, float wid);
-
-// };
 #pragma once
 
-class Rectangle {
-private:
+class Rectangle
+{
+    //private member variables
+    private:
     float length;
     float width;
 
-public:
+
+    public:
+    
+    //default constructor intitilizing variables to 0
     Rectangle();
+
     ~Rectangle();
+    //accesor method for assigning vlues
     void setVAriables(float newlength, float newWidth);
+
+    //accesor methods for retriving values of length and withdth
     float getlength() const;
+
     float getwidth() const;
+
+    //function for calculating  area of rectangle
     float CalculateArea(float len, float wid);
+
 };
